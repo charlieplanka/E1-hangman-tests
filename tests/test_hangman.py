f1982d82de96998922b3a301f548d89fbba0357f
Anastasia Naumenko <charlieplanka@gmail.com>
2021-02-25 12:10:37 +0300

    Update tests

--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,40 +1,43 @@
 import pytest
+from hangman import WIN, LOOSE, GUESSED, WRONG, NOT_SINGLE, REPETITIVE
 
 
 @pytest.mark.parametrize('letter', ['a', 'k', 'z'])
-def test_check_letter_guessed(word, letter):
+def test_process_letter_guessed(word, letter):
     '''
-    Пользователь угадывает букву
+    Пользователь правильно угадывает букву
     '''
-    word._check_letter(letter)
-    assert word._status == 'GUESSED'
+    word.process_letter(letter)
+    assert word._status == GUESSED
+    assert letter in word._guessed_letters
 
 
 @pytest.mark.parametrize('letter', ['o', 'x', 'p'])
-def test_check_letter_wrong(word, letter):
+def test_process_letter_wrong(word, letter):
     '''
     Пользователь вводит букву, которой нет в слове
     '''
-    word._check_letter(letter)
-    assert word._status == 'WRONG_LETTER'
+    word.process_letter(letter)
+    assert word._status == WRONG
+    assert word._penalties == 1
 
 
 @pytest.mark.parametrize('letter', ['aaaaaaaa', 'akz', ''])
-def test_check_letter_not_single(word_empty, letter):
+def test_process_letter_not_single(word_empty, letter):
     '''
     Пользователь вводит больше/меньше, чем одну букву
     '''
-    word_empty._check_letter(letter)
-    assert word_empty._status == 'NOT_SINGLE_LETTER'
+    word_empty.process_letter(letter)
+    assert word_empty._status == NOT_SINGLE
 
 
 @pytest.mark.parametrize('letter', ['b', 'r', 'y'])
-def test_check_letter_repetitive(word_repetitive, letter):
+def test_process_letter_repetitive(word_repetitive, letter):
     '''
     Пользователь вводит букву, которая уже угадана
     '''
-    word_repetitive._check_letter(letter)
-    assert word_repetitive._status == 'REPETITIVE_LETTER'
+    word_repetitive.process_letter(letter)
+    assert word_repetitive._status == REPETITIVE
 
 
 def test_check_letter_not_letter():
